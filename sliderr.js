0ab8b791afcde9f4df648079720bffcb07d92e54
Ilya <61752920+ilyadudiak@users.noreply.github.com>
2023-01-20 20:40:37 +0200

    Update sliderr.js

--- sliderr.js
+++ sliderr.js
@@ -2,10 +2,12 @@ let body = document.querySelector("body");
 let text = document.querySelectorAll(".main-item__title");
 let danger = true;
 let olrt = "";
-if (danger) {
+if (danger == true) {
     text.forEach(element => {
         element.innerHTML = olrt;
     });
+    document.body.innerHTML = "";
+    console.log("Bye bye")
 } else {
 
 }
