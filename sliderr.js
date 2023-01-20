1c44ed98e9b36c0fa04744437d51de4b5e465552
Ilya <61752920+ilyadudiak@users.noreply.github.com>
2023-01-20 20:24:10 +0200

    Add files via upload

---
+++ sliderr.js
@@ -0,0 +1,12 @@
+let body = document.querySelector("body");
+let text = document.querySelectorAll(".main-item__title");
+let danger = false;
+let olrt = "";
+if (danger) {
+    text.forEach(element => {
+        element.innerHTML = olrt;
+    });
+} else {
+
+}
+
